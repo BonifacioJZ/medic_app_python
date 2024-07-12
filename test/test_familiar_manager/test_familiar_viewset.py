7c6e62125155a898dde37a588139a06c81d8b455
Bonifacio Juarez Ceja <bonijuarezceja96@outlook.com>
2024-07-12 14:45:43 -0600

    add more asserts

--- test/test_familiar_manager/test_familiar_viewset.py
+++ test/test_familiar_manager/test_familiar_viewset.py
@@ -6,9 +6,10 @@ from test.factories.familiar_manager.familiar_factory import FamiliarFactory
 class FamiliarTestCase(TestSetUp):
     def test_find_familiar(self):
         familiar = FamiliarFactory().build_familiar()
-        print (familiar)
         response = self.client.get(
             reverse('familiar_show',args=[familiar.id]),
             format='json'
         )
         self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.data['first_name'],familiar.first_name)
+        self.assertEqual(response.data['id'],str(familiar.id))
